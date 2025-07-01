35c50d0f70a03d5ccd294b453549348c339671b7
Md Kaif ALI <130832322+MdKaifAli011@users.noreply.github.com>
2025-07-02 02:39:04 +0530

    Update App.jsx
    All new app.jsx

--- Trigo/src/App.jsx
+++ Trigo/src/App.jsx
@@ -1,42 +1,39 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Navbar from "./components/Navbar";import Navbar from "./components/Navbar";
+
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
-import About from "./pages/About";         
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import About from "./pages/About";
 import Tours from "./pages/Tours";
-import Booking from "./components/Booking";
-import ContactForm from "./components/ContactForm";
-import Success from "./components/Success";
+import Booking from "./pages/Booking";
+import Login from "./pages/Login";
+import SuccessPage from "./pages/SuccessPage"; // if you created one
+import NotFound from "./pages/NotFound"; // optional
 
-const App = () => {
+function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <ToastContainer position="top-right" autoClose={2000} />
-      <Navbar />
-
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/Tours" element={<Tours/>} />
-          <Route path="/booking" element={<Booking/>} />
-          <Route path="/contact" element={<ContactForm/>} />
-
-          <Route path="/success" element={<Success/>} />
-
-
-        </Routes>
-      </main>
-
-      <Footer />
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/tours" element={<Tours />} />
+            <Route path="/booking/:tourId" element={<Booking />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/success" element={<SuccessPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+      <ToastContainer position="top-right" autoClose={3000} />
+    </Router>
   );
-};
+}
 
 export default App;
