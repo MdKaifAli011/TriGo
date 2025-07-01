3d2cd2bb4a01c3b9e67def3af42ca1313c13ad09
Mdkaifali <130832322+MdKaifAli011@users.noreply.github.com>
2025-07-02 02:04:49 +0530

    navbar

--- Trigo/src/App.jsx
+++ Trigo/src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
--- Trigo/src/pages/Home.jsx
+++ Trigo/src/pages/Home.jsx
@@ -16,6 +16,7 @@ const Home = () => {
       <FeaturedTours />
       <ExperienceSection />
       <SubscribeSection />
+     
     </div>
   );
 };
