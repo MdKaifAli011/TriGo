0681ee569843d584c368991da0da5437bbbf3fce
Md Kaif ALI <130832322+MdKaifAli011@users.noreply.github.com>
2025-07-02 02:36:12 +0530

    Update App.jsx

--- Trigo/src/App.jsx
+++ Trigo/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";         
