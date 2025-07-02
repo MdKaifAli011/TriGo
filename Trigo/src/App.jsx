492465fc8c7ebbeb6e73f5781ef95a3b2ffcc3f7
Md Kaif ALI <130832322+MdKaifAli011@users.noreply.github.com>
2025-07-02 12:23:29 +0530

    Update App.jsx fix nav

--- Trigo/src/App.jsx
+++ Trigo/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";         // ✅ Make sure this is here
