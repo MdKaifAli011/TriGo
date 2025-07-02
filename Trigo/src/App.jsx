bee23543c345c1f588a81d21d31246620bf81dc2
Mdkaifali <130832322+MdKaifAli011@users.noreply.github.com>
2025-07-02 11:59:13 +0530

    fix

--- Trigo/src/App.jsx
+++ Trigo/src/App.jsx
@@ -1,39 +1,49 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
 import Home from "./pages/Home";
-import About from "./pages/About";
+import About from "./pages/About";         // ✅ Make sure this is here
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+
+
+
+
+
 import Tours from "./pages/Tours";
 import Booking from "./components/Booking";
-import Login from "./pages/Login";
-import SuccessPage from "./components/Success"; // if you created one
+import ContactForm from "./components/ContactForm";
+import Success from "./components/Success";
 
-
-function App() {
+const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/tours" element={<Tours />} />
-            <Route path="/booking/:tourId" element={<Booking />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/success" element={<Success />} />
-           {/*  <Route path="*" element={<NotFound />} /> */}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-      <ToastContainer position="top-right" autoClose={3000} />
-    </Router>
+    <div className="min-h-screen flex flex-col">
+      <ToastContainer position="top-right" autoClose={2000} />
+      <Navbar />
+
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/Tours" element={<Tours/>} />
+          <Route path="/booking" element={<Booking/>} />
+          <Route path="/contact" element={<ContactForm/>} />
+
+          <Route path="/success" element={<Success/>} />
+
+
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
---
+++ Trigo/src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+const NotFound = () => (
+  <div className="p-8 text-center">
+    <h1 className="text-4xl font-bold text-red-600">404</h1>
+    <p className="text-lg">Oops! Page not found.</p>
+  </div>
+);
+
+export default NotFound;
