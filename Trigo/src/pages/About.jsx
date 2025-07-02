8e4cb33db25788b6a83581145d66860e65969c2c
Md Kaif ALI <130832322+MdKaifAli011@users.noreply.github.com>
2025-07-02 12:30:18 +0530

    Update About.jsx Number change

--- Trigo/src/pages/About.jsx
+++ Trigo/src/pages/About.jsx
@@ -91,11 +91,11 @@ const About = () => {
           </div>
           <div>
             <p className="font-semibold">Phone</p>
-            <p>+91-9130045670</p>
+            <p>+91-9102701502</p>
           </div>
           <div>
             <p className="font-semibold">Address</p>
-            <p>Ghaziabad, India</p>
+            <p>New Delhi, India</p>
           </div>
         </div>
         <button  onClick={() => navigate("/contact")} className="mt-6 bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 transition">
