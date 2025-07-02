73318115fe8af6c84edc889bec08bed7e638ff5e
Mdkaifali <130832322+MdKaifAli011@users.noreply.github.com>
2025-07-02 12:19:47 +0530

    NotFound page add

--- Trigo/src/App.jsx
+++ Trigo/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Navbar from "./components/navbar";
+
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import About from "./pages/About";        
+import About from "./pages/About";         // ✅ Make sure this is here
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
 
 
 
@@ -35,6 +37,7 @@ const App = () => {
           <Route path="/contact" element={<ContactForm/>} />
 
           <Route path="/success" element={<Success/>} />
+          <Route path="*" element={<NotFound/>} />
 
 
         </Routes>
